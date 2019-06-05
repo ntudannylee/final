48f975338be99e84d7c36e9e84dabfcb5cfc95ca
ntudannylee <43718977+ntudannylee@users.noreply.github.com>
2019-06-06 01:10:34 +0800

    新增碰撞事件、七個方塊、顏色陣列

--- js/main.js
+++ js/main.js
@@ -4,19 +4,80 @@ const ctx = canvas.getContext('2d'); //建立繪製物件
 //將ctx繪製放大   (x,y) 20 = 2000%
 ctx.scale(20, 20);
 
-//3x3的二維陣列，讓它有旋轉的軸心
-//T 
-const matrix = [
-    [0, 0, 0],
-    [1, 1, 1],
-    [0, 1, 0],
+const color = [
+    null, //加上null讓value從1~7，因為在for迴圈判斷那邊有用if過濾掉value==0的狀況
+    '#FF3333', //T
+    '#5555FF', //L
+    '#EEEE00', //I
+    '#E93EFF', //J
+    '#4400B3', //O
+    'black', //Z
+    '#33FF33', //S
+
 ]
+
 const player = {
-    position: { x: 5, y: 5 },
-    matrix: matrix,
+    position: { x: 0, y: 0 },
+    matrix: null,
 }
-const matrix_arena = create_arena(12, 20);
+const matrix_arena = create_arena(12, 20); //建立寬12高20的場地
 //console.table(matrix_arena);
+
+//創造tetrix_puzzle，且每個都要讓它有旋轉的軸心
+function create_puzzle(type) {
+    switch (type) {
+        case 'T':
+            return [
+                [0, 0, 0],
+                [1, 1, 1],
+                [0, 1, 0],
+            ];
+            break;
+        case 'L':
+            return [
+                [0, 2, 0],
+                [0, 2, 0],
+                [0, 2, 2],
+            ];
+            break;
+        case 'I':
+            return [
+                [0, 3, 0, 0],
+                [0, 3, 0, 0],
+                [0, 3, 0, 0],
+                [0, 3, 0, 0],
+            ];
+            break;
+        case 'J':
+            return [
+                [0, 4, 0],
+                [0, 4, 0],
+                [4, 4, 0],
+            ];
+            break;
+        case 'O':
+            return [
+                [5, 5],
+                [5, 5],
+            ];
+            break;
+        case 'Z':
+            return [
+                [6, 6, 0],
+                [0, 6, 6],
+                [0, 0, 0],
+            ];
+            break;
+        case 'S':
+            return [
+                [0, 7, 7],
+                [7, 7, 0],
+                [0, 0, 0],
+            ];
+            break;
+    }
+
+}
 
 function create_arena(width, height) { //設定場地
     const matrix = [];
@@ -37,6 +98,20 @@ function merge(matrix_arena, player) { //合併場地與puzzle
     });
 }
 
+function collide(matrix_arena, player) { //  透過player的matrix與arena的matrix來判斷同一列的行有沒有重疊到
+    m = player.matrix;
+    p = player.position;
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 && (matrix_arena[y + p.y] && matrix_arena[y + p.y][x + p.x]) != 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+//繪製背景並呼叫tetris_puzzle畫出一個方塊
 function draw() {
     // 建立線型漸層物件 參數依序為 x0(x起始點)、y0(y起始點)、x1(x結束點)、y1(y結束點)
     var grd = ctx.createLinearGradient(0, 0, 0, 400);
@@ -48,6 +123,8 @@ function draw() {
     ctx.fillStyle = grd;
     // 執行填滿繪製 參數依序為 x、y、width、height
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    tetris_puzzle(matrix_arena, { x: 0, y: 0 });
     tetris_puzzle(player.matrix, player.position);
 }
 
@@ -55,18 +132,31 @@ function tetris_puzzle(matrix, offset) {
     matrix.forEach((row, y) => { //forEach(value of int[i], index of int[])
         row.forEach((value, x) => {
             if (value != 0) {
-                ctx.fillStyle = '#FF3333';
+                ctx.fillStyle = color[value];
                 ctx.fillRect(x + offset.x, y + offset.y, 1, 1);
             }
         });
     });
 }
 
-function tetris_drop() { //tetris 下降 snippet
-    player.position.y++;
-    dropCounter = 0;
+function puzzle_reset() {
+    const puzzle = 'TSZIJLO';
+    var random = Math.floor(Math.random() * 7); //0~6  math.floor是捨五入math.random的值
+    var randomforx = Math.floor(Math.random() * 10); //瘋狂亂下雨模式
+    player.matrix = create_puzzle(puzzle[random]);
+    player.position.y = 0;
+    player.position.x = randomforx;
 }
 
+function tetris_drop() { //tetris 下降及碰撞判斷
+    player.position.y++;
+    dropCounter = 0;
+    if (collide(matrix_arena, player)) {
+        player.position.y--;
+        merge(matrix_arena, player);
+        puzzle_reset();
+    }
+}
 document.addEventListener('keydown', event => {
     button = event.keyCode; //使用者輸入按鍵的key code
     console.log(button);
@@ -82,6 +172,7 @@ document.addEventListener('keydown', event => {
             break;
     }
 })
+
 
 let dropCounter = 0;
 let dropInterval = 1000; //1000毫秒等於1秒
@@ -103,4 +194,5 @@ function update(time = 0) {
     requestAnimationFrame(update); //requestAnimationFrame(callback)
 }
 
-update()
+puzzle_reset();
+update();
